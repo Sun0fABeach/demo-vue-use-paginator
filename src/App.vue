893198eceb6dc6d894bc8c4d45f376406d776862
Markus Appel <markospoparkos@gmail.com>
2025-02-27 13:50:28 +0100

    fresh start with vite 6

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,150 @@
-<script setup lang="ts">
-import Demo from './components/Demo.vue'
-</script>
-
 <template>
-  <Demo />
+  <h1>Demo: vue-use-paginator</h1>
+
+  <form action="" @submit.prevent.stop>
+    <div id="inputs">
+      <label>
+        numButtons:
+      </label>
+      <input type="number" v-model.number="numButtons">
+
+      <label>
+        numItems:
+      </label>
+      <input type="number" v-model.number="numItems">
+
+      <label>
+        pageSize:
+      </label>
+      <select v-model.number="pageSize">
+        <option>5</option>
+        <option>10</option>
+        <option>25</option>
+        <option>50</option>
+        <option>100</option>
+      </select>
+
+      <label>
+        page:
+      </label>
+      <input type="number" v-model.number="page">
+    </div>
+
+    <br>
+    <br>
+
+    <div>numPages: {{ numPages }}</div>
+
+    <br>
+
+    <div>slice: {{ slice }}</div>
+
+    <br>
+
+    <div>hasPrev: {{ hasPrev }}</div>
+    <div>hasNext: {{ hasNext }}</div>
+
+    <br>
+
+    <label>buttons:</label>
+    <ul>
+      <li v-for="(button, idx) in buttons" :key="idx">
+        {{ button }}
+      </li>
+    </ul>
+
+    <label>styled:</label>
+    <ul class="paginator">
+      <li class="control" :class="{ disabled: !hasPrev }" @click="goStart">
+        {{ '<<' }}
+      </li>
+      <li class="control" :class="{ disabled: !hasPrev }" @click="goPrev">
+        {{ '<' }}
+      </li>
+      <li
+        v-for="(button, idx) in buttons"
+        :key="idx"
+        :class="{ 'current-page': button.active }"
+        @click="page = button.page"
+      >
+        {{ button.ellipsis ? '...' : button.page }}
+      </li>
+      <li class="control" :class="{ disabled: !hasNext }" @click="goNext">
+        {{ '>' }}
+      </li>
+      <li class="control" :class="{ disabled: !hasNext }" @click="goEnd">
+        {{ '>>' }}
+      </li>
+    </ul>
+
+  </form>
 </template>
 
+<script setup lang="ts">
+import usePaginator from 'vue-use-paginator'
+
+const {
+  page,
+  pageSize,
+  numPages,
+  numItems,
+  numButtons,
+  slice,
+  buttons,
+  hasPrev,
+  hasNext,
+  goStart,
+  goPrev,
+  goNext,
+  goEnd,
+} = usePaginator({
+  pageSize: 10,
+  numItems: 70,
+  numButtons: 5,
+})
+</script>
+
+<style scoped>
+#inputs {
+  display: inline-grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 1rem;
+}
+#inputs input, #inputs select {
+  box-sizing: border-box;
+  width: 60px;
+}
+
+ul.paginator {
+  display: flex;
+  list-style: none;
+  user-select: none;
+}
+ul.paginator li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+}
+ul.paginator li:not(.current-page):hover {
+  font-weight: bold;
+  border-width: 2px;
+  cursor: pointer;
+}
+ul.paginator li.current-page {
+  color: blue;
+  border-color: blue;
+}
+ul.paginator li.control {
+  border: none;
+  font-weight: 900;
+  cursor: pointer;
+}
+ul.paginator li.control.disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+</style>
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,150 +1,41 @@
-<template>
-  <h1>Demo: vue-use-paginator</h1>
-
-  <form action="" @submit.prevent.stop>
-    <div id="inputs">
-      <label>
-        numButtons:
-      </label>
-      <input type="number" v-model.number="numButtons">
-
-      <label>
-        numItems:
-      </label>
-      <input type="number" v-model.number="numItems">
-
-      <label>
-        pageSize:
-      </label>
-      <select v-model.number="pageSize">
-        <option>5</option>
-        <option>10</option>
-        <option>25</option>
-        <option>50</option>
-        <option>100</option>
-      </select>
-
-      <label>
-        page:
-      </label>
-      <input type="number" v-model.number="page">
-    </div>
-
-    <br>
-    <br>
-
-    <div>numPages: {{ numPages }}</div>
-
-    <br>
-
-    <div>slice: {{ slice }}</div>
-
-    <br>
-
-    <div>hasPrev: {{ hasPrev }}</div>
-    <div>hasNext: {{ hasNext }}</div>
-
-    <br>
-
-    <label>buttons:</label>
-    <ul>
-      <li v-for="(button, idx) in buttons" :key="idx">
-        {{ button }}
-      </li>
-    </ul>
-
-    <label>styled:</label>
-    <ul class="paginator">
-      <li class="control" :class="{ disabled: !hasPrev }" @click="goStart">
-        {{ '<<' }}
-      </li>
-      <li class="control" :class="{ disabled: !hasPrev }" @click="goPrev">
-        {{ '<' }}
-      </li>
-      <li
-        v-for="(button, idx) in buttons"
-        :key="idx"
-        :class="{ 'current-page': button.active }"
-        @click="page = button.page"
-      >
-        {{ button.ellipsis ? '...' : button.page }}
-      </li>
-      <li class="control" :class="{ disabled: !hasNext }" @click="goNext">
-        {{ '>' }}
-      </li>
-      <li class="control" :class="{ disabled: !hasNext }" @click="goEnd">
-        {{ '>>' }}
-      </li>
-    </ul>
-
-  </form>
-</template>
-
 <script setup lang="ts">
-import usePaginator from 'vue-use-paginator'
+import { ref } from 'vue'
 
-const {
-  page,
-  pageSize,
-  numPages,
-  numItems,
-  numButtons,
-  slice,
-  buttons,
-  hasPrev,
-  hasNext,
-  goStart,
-  goPrev,
-  goNext,
-  goEnd,
-} = usePaginator({
-  pageSize: 10,
-  numItems: 70,
-  numButtons: 5,
-})
+defineProps<{ msg: string }>()
+
+const count = ref(0)
 </script>
 
-<style scoped>
-#inputs {
-  display: inline-grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 1rem;
-}
-#inputs input, #inputs select {
-  box-sizing: border-box;
-  width: 60px;
-}
+<template>
+  <h1>{{ msg }}</h1>
 
-ul.paginator {
-  display: flex;
-  list-style: none;
-  user-select: none;
-}
-ul.paginator li {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  width: 50px;
-  height: 50px;
-  border: 1px solid black;
-}
-ul.paginator li:not(.current-page):hover {
-  font-weight: bold;
-  border-width: 2px;
-  cursor: pointer;
-}
-ul.paginator li.current-page {
-  color: blue;
-  border-color: blue;
-}
-ul.paginator li.control {
-  border: none;
-  font-weight: 900;
-  cursor: pointer;
-}
-ul.paginator li.control.disabled {
-  opacity: 0.5;
-  pointer-events: none;
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
 }
 </style>
